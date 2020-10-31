3f1d6da5f6dc1729cbd9d10f2f7f8980392ddf2f
MuhammadRifal22 <rifalmuhammad797@gmail.com>
2020-10-31 15:22:18 +0700

    Add project files.

---
+++ Client_Muhammad Rifal_20180140121/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client_Muhammad_Rifal_20180140121
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            ServiceReference1.MatematikaClient obj = new ServiceReference1.MatematikaClient();
+            double hasilTambah = obj.Tambah(a, b);
+            label3.Text = "Hasil Penjumlahan : " + hasilTambah.ToString();
+            double hasilKurang = obj.Kurang(a, b);
+            label4.Text = "Hasil Pengurangan : " + hasilKurang.ToString();
+            double hasilKali = obj.Kali(a, b);
+            label5.Text = "Hasil Perkalian : " + hasilKali.ToString();
+            double hasilBagi = obj.Bagi(a, b);
+            label6.Text = "Hasil pembagian : " + hasilBagi.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ServerdanClient_Muhammad Rifal_20180140121/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerdanClient_Muhammad_Rifal_20180140121
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = true;
+            label3.Text = "Klik OFF Untuk Mematikan Server";
+            label2.Text = "Server ON";
+
+            Server server = new Server();
+            server.OnServer();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+            label3.Text = "Klik ON Untuk Menjalankan Server";
+            label2.Text = "Server OFF";
+
+            Server server = new Server();
+            server.OffServer();
+        }
+    }
+}
